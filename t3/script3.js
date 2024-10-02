3069eb7c12539dd245b433244f04d06f4860174a
Pradhvinigowda <150103545+Pradhvinigowda@users.noreply.github.com>
2024-10-02 11:44:07 +0530

    Add files via upload

---
+++ t3/script3.js
@@ -0,0 +1,80 @@
+const cells = document.querySelectorAll('[data-cell]');
+const X_CLASS = 'X';
+const O_CLASS = 'O';
+let oTurn = false;
+
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+startGame();
+
+function startGame() {
+    cells.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+    }
+}
+
+function placeMark(cell, currentClass) {
+    cell.textContent = currentClass;
+    cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+    oTurn = !oTurn;
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass);
+        });
+    });
+}
+
+function endGame(draw) {
+    if (draw) {
+        alert("It's a draw!");
+    } else {
+        alert(`${oTurn ? "O's" : "X's"} Wins!`);
+    }
+    resetGame();
+}
+
+function isDraw() {
+    return [...cells].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
+}
+
+function resetGame() {
+    cells.forEach(cell => {
+        cell.textContent = '';
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    oTurn = false;
+}
